97e6ce02d4dce7de00f3cfb1dd87adcdce689322
Cesar-64bit <cesar.1ng.1nf@gmail.com>
2021-10-09 17:37:17 -0600

    Primer cambio

---
+++ Linked List/Simple Linked List/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+struct node {
+	int data;
+	struct node *next;
+};
+
+typedef struct node *NODE;
+
+int main(void) {
+	printf("\n Hola Mundo");
+}
