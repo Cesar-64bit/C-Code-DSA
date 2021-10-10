8133ca63e7625e2dbc8e7169ca96c0d6ba3b715a
Cesar-64bit <cesar.1ng.1nf@gmail.com>
2021-10-09 22:25:59 -0600

    funcion agregar, eliminar, mostrar hechas

--- Linked List/Simple Linked List/main.c
+++ Linked List/Simple Linked List/main.c
@@ -9,6 +9,97 @@ struct node {
 
 typedef struct node *NODE;
 
+void add_to_list(NODE *head, int data) {
+	struct node *new_node;
+	new_node = (struct node*)malloc(sizeof(struct node));
+
+	if(new_node == NULL) {
+		puts("\n Insufficient memory");
+		getch();
+	}
+	else {
+		new_node->data = data;
+		new_node->next = NULL;
+
+		if(*head == NULL)
+			*head = new_node;
+		else {
+			struct node *search;
+			search = *head;
+
+			while(search->next != NULL)
+				search = search->next;
+
+			search->next = new_node;
+		}
+	}
+}
+
+bool empty_list(NODE *head) {
+	if(*head == NULL)
+		return true;
+
+	return false; 
+}
+
+void delete_element(NODE *head, int data) {
+	if(empty_list(head) == true) {
+		puts("\n Empty list");
+		getch();
+	}
+	else {
+		struct node *aux_node, *previous;
+		aux_node = *head;
+		previous = *head;
+
+		if(aux_node->data == data) {
+			*head = aux_node->next;
+			free(aux_node);
+		}
+		else {
+			while(aux_node->next != NULL) {
+				if(aux_node->data == data) {
+					previous->next = aux_node->next;
+					free(aux_node);
+					break;
+				}
+				previous = aux_node;
+				aux_node = aux_node->next;
+			}
+
+			if(aux_node->data == data) {
+				previous->next = aux_node->next;
+				free(aux_node);
+			}
+			else {
+				puts("\n No data found");
+				getch();
+			}
+		}
+	}
+}
+
+void show_list(NODE *head) {
+	if(empty_list(head) == true) {
+		puts("\n Empty list");
+		getch();
+	}
+	else {
+		struct node *aux_node;
+		aux_node = *head;
+
+		while(aux_node->next != NULL) {
+			printf(" %d <-", aux_node->data);
+			aux_node = aux_node->next;
+		}
+		printf(" %d <-", aux_node->data);
+	}
+}
+
 int main(void) {
-	printf("\n Hola Mundo");
+	NODE head = NULL;
+
+	add_to_list(&head, 28);
+	show_list(&head);
+	delete_element(&head, 39);
 }
